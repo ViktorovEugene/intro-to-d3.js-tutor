df46cb569bff5b847ddd1d9cd3660229d8a57cfc
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-03-09 17:35:37 +0200

    Add the y-axis

--- js/parts_of_a_graph.js
+++ js/parts_of_a_graph.js
@@ -1,8 +1,3 @@
-/**
- * Created by jeka on 09.03.16.
- */
-/* it renders scales to the page */
-
 var numbers = [ 5, 4, 10, 1 ],
     data = [
       { date: '2014-01-01', amount: 10 },
@@ -10,7 +5,7 @@ var numbers = [ 5, 4, 10, 1 ],
       { date: '2014-03-01', amount: 40 },
       { date: '2014-04-01', amount: 80 }
     ];
-
+    
 var y = d3.scale.linear()
   .domain([0, 80]) // $0 to $80
   .range([200, 0]); // Seems backwards because SVG is y-down
@@ -21,17 +16,30 @@ var x = d3.time.scale()
       new Date(Date.parse('2014-04-01'))
     ])
     .range([0, 300]);
-
+    
 var xAxis = d3.svg.axis()
   .scale(x)         // x is the d3.time.scale()
   .orient('bottom') // the ticks go below the graph
   .ticks(4);        // specify the number of ticks
+  
+var yAxis = d3.svg.axis()
+  .scale(y)         // x is the d3.time.scale()
+  .orient('left') // the ticks go below the graph
+  .ticks(4);        // specify the number of ticks
 
-var svg = d3.select('body')
+var svg = d3.select('div')
   .append('svg')        // create an <svg> element
-    .attr('width', 400) // set its dimensions
-    .attr('height', 150);
+    .attr('width', 400) // set its dimentions
+    .attr('height', 350)
+  .append("g")
+    .attr("transform", "translate(10,10)");
 
 svg.append('g')            // create a <g> element
-  .attr('class', 'x axis') // specify classes
-  .call(xAxis);            // let the axis do its thing
+    .attr('class', 'x axis')// specify classes
+    .attr("transform", "translate(30," + 200 + ")")
+    .call(xAxis);            // let the axis do its thing
+
+svg.append('g')
+    .attr('class', 'y axis')
+    .attr("transform", "translate(30,0)")
+    .call(yAxis)
