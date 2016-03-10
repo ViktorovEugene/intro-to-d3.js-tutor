46b6549a651b8a2f99c310e985b25d30ec57f2f8
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-03-10 16:22:35 +0200

    Make the "jump transition" with data changing
    Now the graph is updating after the alert call by toggling
    to other data array.

--- js/data_binding.js
+++ js/data_binding.js
@@ -1,82 +1,60 @@
-var svg = d3.select('svg')
-    .attr('width', 320)
-    .attr('height', 85)
-  .append('g')
-    .attr('transform', 'translate(10,10)');
+var sales,
+    svg = d3.select('svg')
+        .attr('width', 320)
+        .attr('height', 85)
+      .append('g')
+        .attr('transform', 'translate(10,10)');
 
-/* The Key Function */
+/* Transition */
 
-// This is the initial data to build the graph
-var sales1 = [
-  { product: 'Hoodie', count: 7 },
-  { product: 'Jacket', count: 6 }
-];
-
-// This is the updated data, that will be used to rearrange the initial graph
-var sales2 = [
-  { product: 'Jacket',  count: 6 }, // same
-  { product: 'Snuggie', count: 9 }  // new
+// data storage
+days = [
+    [
+      { product: 'Hoodie', count: 10 },
+      { product: 'Jacket', count: 3 },
+      { product: 'Snuggie', count: 2 }
+    ],
+    [
+      { product: 'Hoodie', count: 16 },
+      { product: 'Jacket', count: 7 },
+      { product: 'Snuggie', count: 8 }
+    ]
 ];
 
 // Create the scales for further visualisation
 var x = d3.scale.linear()
   .range([0, 300])
-  .domain([0, 10]);
+  .domain([0, 20]);
 var y = d3.scale.ordinal()
   .rangeRoundBands([0, 75])
   .domain(['Hoodie', 'Jacket', 'Snuggie']);
 
+function toggle() {
+    sales = (sales == days[0]) ? days[1] : days[0];
+    update();
+}
 
-// Bind the initial data from "sales1"
-var rects = svg.selectAll('rect')
-  .data(sales1, function(d, i) { return d.product; } );
+function update() {
+    var rects = svg.selectAll('rect')
+        .data(sales, function(d, i) {
+            return d.product
+        });
 
-alert(rects.size() + ' - initial number of rect\'s tags.\nThe data is ' +
-    'bound, but isn\'t appended yet.');
+    // When we enter, we just want to create the rect,
+    rects.enter()
+        .append('rect');
 
-// 2 -- first join, adds two new elements
-rects.enter().append('rect')
-  .attr('x', x(0))
-  .attr('y', function(d, i) {
-    return y(d.product);
-  })
-  .attr('height', y.rangeBand())
-  .attr('width', function(d, i) {
-    return x(d.count);
-  });
+    rects
+        .attr('x', x(0))
+        .attr('y', function(d, i) {
+            return y(d.product);
+        })
+        .attr('height', y.rangeBand())
+        .attr('width', function(d, i) {
+            return x(d.count);
+        });
+}
 
-alert(rects.size() + ' - rect\'s after appending.\nYou can see the new ' +
-    'elements now.');
-
-
-/* Bind the new array in to elements - one is like as previous one, but other
- * is brand new.
- * We should rearrange the graph with this new data.
- */
-var nextrects = rects
-  .data(sales2, function(d, i) { return d.product; });
-
-alert('Now we are using the new array of data. It has the single unchanged ' +
-    'element and one brand new element.\n' + nextrects.exit().size() +
-    ' - this is the elements from "exit()" call. ' +
-    'This is the element that was in the previous array but not exists in ' +
-    'the new array of data.\n' +
-    'Let\'s delete it from the screen!');
-// 1 -- one element to remove
-nextrects.exit().remove();
-alert('One selected element was removed! Now the brand new element will' +
-    ' be added!');
-
-
-// Append one new element
-nextrects.enter().append('rect')
-  .attr('x', x(0))
-  .attr('y', function(d, i) {
-    return y(d.product);
-  })
-  .attr('height', y.rangeBand())
-  .attr('width', function(d, i) {
-    return x(d.count);
-  });
-
-alert(nextrects.size() + " - elements on the screen after appending!");
+toggle();
+alert('Let\'s toggle again!');
+toggle();
